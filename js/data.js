cb949d8a6f3d3b692b88dd2d15d34133bedaa54f
Никита <ageev33nik@gmail.com>
2023-04-27 00:02:49 +0300

    Разбил на модули

---
+++ js/data.js
@@ -0,0 +1,21 @@
+import {getRandomInteger, generateComments} from './util.js';
+
+
+function generatePosts() {
+  const objectArray = [];
+
+  for(let i = 0; i <= 25; i++) {
+    const crtImage = {
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: `Я тут был ${i} раз!`,
+      likes: getRandomInteger(15, 200),
+      comments: generateComments(),
+    };
+    objectArray.push(crtImage);
+  }
+  return objectArray;
+}
+
+
+export {generatePosts};
