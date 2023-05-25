92ce9c64b15ee2f0054e15ad9c7a237e8f933ede
Никита <ageev33nik@gmail.com>
2023-05-26 00:21:47 +0300

    +модуль rendering

---
+++ js/rendering.js
@@ -0,0 +1,30 @@
+import { generatePosts } from './data.js';
+
+const pictureContainer = document.querySelector('#picture').content;
+
+const pictureItem = pictureContainer.querySelector('.picture');
+
+const pictures = document.querySelector('.pictures');
+
+const fragment = document.createDocumentFragment();
+
+const dataList = generatePosts();
+
+for(let i = 0; i < dataList.length; i++) {
+  const element = pictureItem.cloneNode(true);
+  element.classList.add('picture');
+
+  const pictureImg = pictureItem.querySelector('.picture__img');
+  const pictureComment = pictureItem.querySelector('.picture__comments');
+  const pictureLike = pictureItem.querySelector('.picture__likes');
+
+  pictureImg.src = dataList[i].url;
+  pictureComment.textContent = dataList[i].comments;
+  pictureLike.textContent = dataList[i].likes;
+  fragment.appendChild(element);
+}
+
+pictures.appendChild(fragment);
+
+
+export { };
