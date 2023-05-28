66641dbebea078bb35fc482c21ebc1e67b9d17ee
Никита <ageev33nik@gmail.com>
2023-05-28 12:58:28 +0300

    add functon

--- js/rendering.js
+++ js/rendering.js
@@ -4,27 +4,26 @@ const pictureContainer = document.querySelector('#picture').content;
 
 const pictureItem = pictureContainer.querySelector('.picture');
 
-const pictures = document.querySelector('.pictures');
-
 const fragment = document.createDocumentFragment();
 
 const dataList = generatePosts();
 
-for(let i = 0; i < dataList.length; i++) {
-  const element = pictureItem.cloneNode(true);
-  element.classList.add('picture');
 
-  const pictureImg = element.querySelector('.picture__img');
-  const pictureComment = element.querySelector('.picture__comments');
-  const pictureLike = element.querySelector('.picture__likes');
+function fillingСards() {
+  for(let i = 0; i < dataList.length; i++) {
+    const element = pictureItem.cloneNode(true);
+    element.classList.add('picture');
 
-  pictureImg.src = dataList[i].url;
-  pictureComment.textContent = dataList[i].comments;
-  pictureLike.textContent = dataList[i].likes;
-  fragment.appendChild(element);
+    const pictureImg = element.querySelector('.picture__img');
+    const pictureComment = element.querySelector('.picture__comments');
+    const pictureLike = element.querySelector('.picture__likes');
+
+    pictureImg.src = dataList[i].url;
+    pictureComment.textContent = dataList[i].comments;
+    pictureLike.textContent = dataList[i].likes;
+    fragment.appendChild(element);
+  }
+  return fragment;
 }
 
-pictures.appendChild(fragment);
-
-
-export { };
+export { fillingСards };
